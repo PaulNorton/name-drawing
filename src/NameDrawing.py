207a85525cab83ef5531ec1d6102985ed9b5f5f8
Joe Norton <jjnorton@pbs.org>
2011-10-24 16:03:31 -0400

    Added family functionality

--- src/NameDrawing.py
+++ src/NameDrawing.py
@@ -1,7 +1,36 @@
-class NameDrawing:
-	def __init__(self, people):
-		self.people = people
-		self.names = [ person['name'] for person in people ]
+import random
 
-	def draw(self):
-		return { 'Tom':'Ginny', 'Richard':'Harry', 'Susan': 'Albus', 'Harry':'Tom', 'Ginny':'Richard', 'Albus':'Susan' }
+class NameDrawing:
+    def __init__(self, list):
+        self.names = list
+            
+
+    def draw(self):
+        if len(self.names) < 2:
+            return 'Not enough people in the list!'
+        while True:
+            restart = False
+            results = {}
+            giftees = list(self.names)
+            for gifter in self.names:
+                while True:
+                    random_index = random.randint(0, (len(giftees) - 1))
+                    possible_giftee = giftees[random_index]
+                    if possible_giftee['name'] not in gifter['exclude'] and possible_giftee != gifter:
+                        break
+                    else:
+                        restart = True
+                        for remaining_giftee in giftees:
+                            if remaining_giftee['name'] not in gifter['exclude'] and remaining_giftee != gifter:
+                                restart = False
+                        if restart == True:
+                            break
+                results[gifter['name']] = giftees.pop(random_index)['name']
+            if restart == False:
+                print results
+                #return results
+                break
+                
+people = [{'exclude': [], 'name': 'Tom'}, {'exclude': ['Susan'], 'name': 'Richard'}, {'exclude': ['Richard'], 'name': 'Susan'}, {'exclude': ['Ginny', 'Albus'], 'name': 'Harry'}, {'exclude': ['Harry', 'Albus'], 'name': 'Ginny'}, {'exclude': ['Harry', 'Ginny'], 'name': 'Albus'}]
+test = NameDrawing(people)
+print test.draw()
--- src/NameDrawingTest.py
+++ src/NameDrawingTest.py
@@ -7,8 +7,10 @@ class TestNameDrawing(unittest.TestCase):
 			['Tom'], ['Richard', 'Susan'], ['Harry', 'Ginny', 'Albus']
 		]
 		people = []
+		self.list_of_names = []
 		for unit in self.familyUnits:
 			for person in unit:
+				self.list_of_names.append(person)
 				exclude = list(unit)
 				exclude.remove(person)
 				people.append({'name': person, 'exclude': exclude})
@@ -18,18 +20,18 @@ class TestNameDrawing(unittest.TestCase):
 	def test_drawing__everybodyIsGettingSomebodyAGift(self):
 		results = self.drawing.draw()
 		for person in self.drawing.names:
-			self.assertTrue(person in results.keys(), person+" was in the list, but is not getting anybody a gift")
+			self.assertTrue(person['name'] in results.keys(), person['name']+" was in the list, but is not getting anybody a gift")
 
 	def test_drawing__everybodyIsGettingAGift(self):
 		results = self.drawing.draw()
 		for person in self.drawing.names:
-			self.assertTrue(person in results.values(), person+" was in the list, but is not receiving a gift")
+			self.assertTrue(person['name'] in results.values(), person['name']+" was in the list, but is not receiving a gift")
 
 	def test_drawing__noRandomPeopleInDrawing(self):
 		results = self.drawing.draw()
 		for gifter, giftee in results.items():
-			self.assertTrue(gifter in self.drawing.names, gifter+" was not in the list, but is getting "+giftee+" a gift!")
-			self.assertTrue(giftee in self.drawing.names, giftee+" was not in the list, but is getting a gift from "+gifter)
+			self.assertTrue(gifter in self.list_of_names, gifter+" was not in the list, but is getting "+giftee+" a gift!")
+			self.assertTrue(giftee in self.list_of_names, giftee+" was not in the list, but is getting a gift from "+gifter)
 
 	def test_drawing__nobodyGetsThemselfAGiftLikeKevinFromTheOffice(self):
 		results = self.drawing.draw()
