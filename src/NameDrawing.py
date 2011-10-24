b785c9b7819166839ede70cbe5ea33ea96d3789d
jnorton001 <joey@nortoncrew.com>
2011-10-24 13:36:52 -0300

    Joe's edits

--- src/NameDrawing.py
+++ src/NameDrawing.py
@@ -1,6 +1,31 @@
-class NameDrawing:
-	def __init__(self, names):
-		self.names = names
+import random
 
-	def draw(self):
-		return { 'Tom':'Richard', 'Richard':'Harry', 'Harry':'Tom' }
+class NameDrawing:
+    def __init__(self, names):
+        self.names = []
+        for name in names:
+            while name in self.names:
+                name += '*'
+            self.names.append(name)
+        
+
+    def draw(self):
+        if len(self.names) < 2:
+            return 'Not enough people in the list!'
+        while True:
+            restart = False
+            results = {}
+            giftees = list(self.names)
+            for a in self.names:
+                while True:
+                    c = random.randint(0, (len(giftees) - 1))
+                    b = giftees[c]
+                    if b != a:
+                        break
+                    elif len(giftees) == 1 and a == giftees[0]:
+                        restart = True
+                        break
+                results[a] = giftees.pop(c)
+            if restart == False:
+                return results
+                break
